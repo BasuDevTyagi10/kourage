46ee356972362c2b980dd6df586e68d4b6576df4
Saksham <saksham@Sakshams-MacBook-Air.local>
2023-02-06 15:47:05 +0530

    feat:remove more substring from the reviews

--- helper.js
+++ helper.js
@@ -1,9 +1,12 @@
 const { EmbedBuilder } = require("discord.js");
-const { getReviews } = require("./scrapper");
-const { channelId } = require("./config.json")
+const path = require("path");
+const fs = require("fs");
+require('dotenv').config()
 
-const INTERVAL_TIME = 6 * 60 * 60 * 1000;
-let REVIEWS = [];
+const logger = require("./logger");
+const { getReviews } = require("./scrapper");
+const { info } = require("console");
+
 
 /**
  * This function fetch the google reviews after every 12 hours and send the latest reviews on discord channel.
@@ -13,52 +16,130 @@ let REVIEWS = [];
 const startInterval = async (discordClient) => {
   try {
     const { reviews } = await getReviews();
-    if (REVIEWS?.length === 0) {
-      REVIEWS = reviews;
-      sendMessage(REVIEWS, discordClient);
-    } else if (reviews?.length > REVIEWS?.length) {
-      const latestReviewsList = [];
-      for (let i = 0; i < reviews?.length; i++) {
-        if (i > reviews?.length - REVIEWS?.length) {
+    if (reviews?.length) {
+      logger.info(
+        "Scrapping successfully done!"
+      );
+    }
+    const fileReviews = await readFromFile();
+    if (fileReviews === null) {
+      logger.info(
+        "Sending All review first time"
+      );
+      sendMessage(reviews, discordClient);
+      writeInFile(reviews);
+    } else if (reviews?.length > fileReviews.length) {
+      writeInFile(reviews);
+      const noOfLatestReview = reviews?.length - fileReviews?.length;
+      let count = 0;
+      let latestReviewsList = [];
+
+      for (let i = 0; i < reviews.length; i++) {
+        let flag = false;
+        for (let j = 0; j < fileReviews.length; j++) {
+          if (reviews[i]?.user?.name === fileReviews[j]?.user?.name) {
+            flag = true;
+          }
+        }
+        if (!flag) { latestReviewsList.push(reviews[i]); count++; }
+        if (count > noOfLatestReview) {
           break;
         }
-        latestReviewsList.push(reviews[reviews?.length - i - 1]);
       }
-      if (latestReviewsList?.length)
-        sendMessage(latstReviewsList, discordClient);
+      logger.info(`Find ${latestReviewsList.length} latest review`);
+      sendMessage(latestReviewsList, discordClient);
     }
-    setTimeout(startInterval, INTERVAL_TIME);
+    setTimeout(startInterval, 43200000);
   } catch (error) {
-    console.log(error);
+    logger.error("Error in startInterval:", error);
   }
 };
 
 /**
- * This function sends review message to the specified channel
- * @param {Object} disgordClient Object of discord client.
- * @param {Array} reviews "Array of reviews."
+ * Send reviews to discord channel.
+ * @param {*} reviews Array of reviews objects.
+ * @param {*} discordClient Discord client object.
  */
-const sendMessage = async (reviews, discordClient) => {
-  if (reviews?.length) {
-    const channel = discordClient.channels.cache.get(channelId);
-    reviews?.forEach((element) => {
-      if (!element?.snippet) element["snippet"] = "None";
-      if (element?.user?.name) {
-        const reviewEmbed = new EmbedBuilder()
-          .setColor(0x0099ff)
-          .setURL(element?.user?.thumbnail)
-          .setTitle("⭐".repeat(element?.rating))
-          .setAuthor({
-            name: element?.user?.name,
-            iconURL: element?.user?.thumbnail,
-            url: element?.user?.link,
-          })
-          .setDescription(element?.snippet);
 
-        channel.send({ embeds: [reviewEmbed] });
-      }
-    });
+const sendMessage = async (reviews, discordClient) => {
+  try {
+    if (reviews?.length) {
+      logger.info("Get latest review array");
+    const channel = discordClient.channels.cache.get(process.env.CHANNEL_ID);
+      reviews?.forEach(async (element) => {
+        const redirectUrl = element?.user?.thumbnail ? element?.user?.thumbnail : element?.user?.link;
+        if (!element?.snippet) element["snippet"] = "None";
+        if (element?.user?.name) {
+          const reviewEmbed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle("⭐".repeat(element?.rating))
+            .setAuthor({
+              name: element?.user?.name,
+              iconURL: element?.user?.url,
+              url: redirectUrl,
+            })
+            .setDescription(element?.snippet);
+          const res = await channel.send({ embeds: [reviewEmbed] });
+          if (res.status === 200) {
+            logger.info("Embed send successfully!")
+          }
+        }
+      });
+      logger.info("Reviews send successfully on discord channel");
+    }
+  } catch (error) {
+    logger.error("Error in 'sendMessage' func and error is:", error);
   }
 };
+
+const readFromFile = () => {
+  const filePath = path.join(__dirname, "review.json");
+  return new Promise((resolve, reject) => {
+    try {
+      fs.readFile(filePath, "utf8", async (err, jsonString) => {
+        if (err) {
+          logger.error(
+            "Error in readFromFile func during file reading and error is",
+            err
+          );
+          return resolve(null);
+        } else {
+          let data = null;
+          if (jsonString) data = JSON.parse(jsonString);
+          logger.info("File read successfully!");
+          return resolve(data);
+        }
+      });
+    } catch (error) {
+      logger.error("Error in readFromFile func and error is", error);
+      resolve(null);
+    }
+  });
+};
+
+const writeInFile = (data) => {
+  const filePath = path.join(__dirname, "review.json");
+  return new Promise((resolve, reject) => {
+    try {
+      fs.writeFile(filePath, JSON.stringify(data), (err) => {
+        if (err) {
+          logger.error("Error in writeInFile func and error is:", err);
+        } else {
+          logger.info("File write successfully!");
+          resolve(true);
+        }
+      });
+    } catch (error) {
+      logger.error("Error in writeInFile func and error is", error);
+      resolve(false);
+    }
+  });
+};
+
+const sleep = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  })
+}
 
 module.exports = { startInterval };
--- scrapper.js
+++ scrapper.js
@@ -1,104 +1,149 @@
 const puppeteer = require("puppeteer");
+const logger = require("./logger");
 
 const placeUrl =
   "https://www.google.com/maps/place/Koders/@30.3307776,77.9591553,17z/data=!3m1!4b1!4m5!3m4!1s0x39092b338ee9e6f1:0x5964dd90fecf95d2!8m2!3d30.3307776!4d77.961344";
 
 async function scrollPage(page, scrollContainer) {
-  let lastHeight = await page.evaluate(
-    `document.querySelector("${scrollContainer}").scrollHeight`
-  );
-  while (true) {
-    await page.evaluate(
-      `document.querySelector("${scrollContainer}").scrollTo(0, document.querySelector("${scrollContainer}").scrollHeight)`
-    );
-    await page.waitForTimeout(4000);
-    let newHeight = await page.evaluate(
+  try {
+    logger.info("scrollPage func running!");
+    let lastHeight = await page.evaluate(
       `document.querySelector("${scrollContainer}").scrollHeight`
     );
-    if (newHeight === lastHeight) {
-      break;
+    while (true) {
+      await page.evaluate(
+        `document.querySelector("${scrollContainer}").scrollTo(0, document.querySelector("${scrollContainer}").scrollHeight)`
+      );
+      await page.waitForTimeout(3000);
+      let newHeight = await page.evaluate(
+        `document.querySelector("${scrollContainer}").scrollHeight`
+      );
+      if (newHeight === lastHeight) {
+        break;
+      }
+      lastHeight = newHeight;
     }
-    lastHeight = newHeight;
+  } catch (error) {
+    logger.error("Error in scrollPage func and error is:", error);
   }
 }
 
 async function getReviewsFromPage(page) {
-  const reviews = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".jftiEf")).map((el) => {
-      return {
-        user: {
-          name: el.querySelector(".d4r55")?.textContent.trim(),
-          link: el.querySelector(".WNxzHc a")?.getAttribute("href"),
-          thumbnail: el.querySelector(".NBa7we")?.getAttribute("src"),
-          localGuide:
-            el.querySelector(".RfnDt span:first-child")?.style.display ===
-            "none"
-              ? undefined
-              : true,
-          reviews: parseInt(
-            el
-              .querySelector(".RfnDt span:last-child")
-              ?.textContent.replace("·", "")
+  try {
+    const reviews = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll(".jftiEf")).map((el) => {
+        return {
+          user: {
+            name: el.querySelector(".d4r55")?.textContent.trim(),
+            link: el.querySelector(".WNxzHc a")?.getAttribute("href"),
+            url: el.querySelector(".NBa7we")?.getAttribute("src"),
+            localGuide:
+              el.querySelector(".RfnDt span:first-child")?.style.display ===
+              "none"
+                ? undefined
+                : true,
+            reviews: parseInt(
+              el
+                .querySelector(".RfnDt span:last-child")
+                ?.textContent.replace("·", "")
+            ),
+          },
+          rating: parseFloat(
+            el.querySelector(".kvMYJc")?.getAttribute("aria-label")
           ),
-        },
-        rating: parseFloat(
-          el.querySelector(".kvMYJc")?.getAttribute("aria-label")
-        ),
-        date: el.querySelector(".rsqaWe")?.textContent.trim(),
-        snippet: el.querySelector(".MyEned")?.textContent.trim(),
-        likes: parseFloat(
-          el.querySelector(".GBkF3d:nth-child(2)")?.getAttribute("aria-label")
-        ),
-        images: Array.from(el.querySelectorAll(".KtCyie button")).length
-          ? Array.from(el.querySelectorAll(".KtCyie button")).map((el) => {
-              return {
-                thumbnail: getComputedStyle(el).backgroundImage.slice(5, -2),
-              };
-            })
-          : undefined,
-        date: el.querySelector(".rsqaWe")?.textContent.trim(),
-      };
+          date: el.querySelector(".rsqaWe")?.textContent.trim(),
+          snippet: el.querySelector(".MyEned")?.textContent.trim(),
+          likes: parseFloat(
+            el.querySelector(".GBkF3d:nth-child(2)")?.getAttribute("aria-label")
+          ),
+          images: Array.from(el.querySelectorAll(".KtCyie button")).length
+            ? Array.from(el.querySelectorAll(".KtCyie button")).map((el) => {
+                return {
+                  thumbnail: getComputedStyle(el).backgroundImage.slice(5, -2),
+                };
+              })
+            : undefined,
+          date: el.querySelector(".rsqaWe")?.textContent.trim(),
+        };
+      });
     });
-  });
-  return reviews;
+    return reviews;
+  } catch (error) {
+    logger.error("Error in 'getReviewsFromPage' function and error is:", error);
+  }
 }
 
-async function fillPlaceInfo(page) {
-  const placeInfo = await page.evaluate(() => {
-    return {
-      title: document.querySelector(".DUwDvf")?.textContent.trim(),
-      address: document
-        .querySelector("button[data-item-id='address']")
-        ?.textContent.trim(), // data-item-id attribute may be different if the language is not English
-      rating: (() => {
-        const str = document.querySelector("div.F7nice")?.textContent.trim();
-        return str.substring(0, 3);
-      })(),
-      reviews: (() => {
-        const str = document.querySelector("div.F7nice")?.textContent.trim();
-        return str.substring(3, str.length);
-      })(),
-    };
-  });
-  return placeInfo;
+async function getPageUrl(page) {
+  try {
+    logger.info("Start scrapping thumbnail...!");
+    return await page.evaluate(async () => {
+      const urls = [];
+      const tempArr = [...document.querySelectorAll(".GBkF3d")];
+      for (let i = 0; i < tempArr.length; i++) {
+        if (i % 2 !== 0) {
+          tempArr[i].click();
+          function sleep(time) {
+            return new Promise((resolve) => {
+              setTimeout(resolve, time);
+            });
+          }
+          await sleep(3000);
+          urls.push(document.querySelectorAll(".vrsrZe")[0].value);
+          document.querySelectorAll(".AmPKde")[0].click();
+        }
+      }
+      return urls;
+    });
+  } catch (error) {
+    logger.error("Error in 'getPageUrl' function and error is:", error);
+  }
 }
+
+// Currently Out of scop
+// async function fillPlaceInfo(page) {
+//   const placeInfo = await page.evaluate(() => {
+//     return {
+//       title: document.querySelector(".DUwDvf")?.textContent.trim(),
+//       address: document
+//         .querySelector("button[data-item-id='address']")
+//         ?.textContent.trim(), // data-item-id attribute may be different if the language is not English
+//       rating: (() => {
+//         const str = document.querySelector("div.F7nice")?.textContent.trim();
+//         return str.substring(0, 3);
+//       })(),
+//       reviews: (() => {
+//         const str = document.querySelector("div.F7nice")?.textContent.trim();
+//         return str.substring(3, str.length);
+//       })(),
+//     };
+//   });
+//   return placeInfo;
+// }
 
 async function getLocalPlaceReviews() {
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      executablePath:
-        "/usr/bin/google-chrome",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
+    let browser = null;
+    if (process.platform === "darwin") {
+      browser = await puppeteer.launch({
+        headless: true,
+        executablePath:
+          "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
+    } else {
+      browser = await puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
+    }
 
     const page = await browser.newPage();
 
     await page.setDefaultNavigationTimeout(60000);
     await page.goto(placeUrl);
     await page.waitForSelector(".DUwDvf");
-
-    const placeInfo = await fillPlaceInfo(page);
+    // Use if wants to get place info
+    // const placeInfo = await fillPlaceInfo(page);
     await page.click(".F7nice");
     await page.waitForTimeout(2000);
     await page.waitForSelector(".W1neJ");
@@ -106,17 +151,38 @@ async function getLocalPlaceReviews() {
       page,
       ".w6VYqd > .tTVLSc > .k7jAl > .e07Vkf > .aIFcqe > .m6QErb > .m6QErb"
     );
-    const reviews = await getReviewsFromPage(page);
-
+    let reviews = await getReviewsFromPage(page);
+    if (reviews?.length) {
+      const url = await getPageUrl(page);
+      reviews = reviews.map((item, i) => {
+        if (i < url?.length)
+          return {
+            ...item,
+            snippet: item?.snippet.replace("More", ""),
+            user: {
+              ...item.user,
+              thumbnail: url[i],
+            },
+          };
+        else
+          return {
+            ...item,
+            user: {
+              ...item.user,
+              thumbnail: item?.user?.link,
+            },
+          };
+      });
+    } else reviews = [];
     await browser.close();
-    return { placeInfo, reviews };
+    return { placeInfo: {}, reviews };
   } catch (error) {
-    console.warn(error);
+    logger.error("Error in func getLocalPlaceReviews and error is:", error);
   }
 }
 
 const getReviews = async () => {
+  logger.info("Scrapping start");
   return await getLocalPlaceReviews();
 };
-
 module.exports = { getReviews };
